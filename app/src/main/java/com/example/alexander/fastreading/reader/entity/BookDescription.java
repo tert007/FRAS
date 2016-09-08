ae3ea8c65d705f5e9960071bc7fd26afd5d7d8c0
Alexander Safonov <tert007@gmail.com>
2016-09-09 00:12:20 +0300

    library+

---
+++ app/src/main/java/com/example/alexander/fastreading/reader/entity/BookDescription.java
@@ -0,0 +1,144 @@
+package com.example.alexander.fastreading.reader.entity;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+/**
+ * Created by Alexander on 03.09.2016.
+ */public class BookDescription implements Parcelable {
+
+    private long id;
+
+    private float progress;
+
+    private String title;
+    private String author;
+    private String language;
+
+    private String type;
+
+    private String filePath;
+
+    private boolean itsFavorite;
+
+    private String coverImagePath;
+
+    public BookDescription(){
+
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public void setFilePath(String filePath) {
+        this.filePath = filePath;
+    }
+
+    public boolean itsFavorite() {
+        return itsFavorite;
+    }
+
+    public void setFavorite(boolean favorite) {
+        this.itsFavorite = favorite;
+    }
+
+    public String getCoverImagePath() {
+        return coverImagePath;
+    }
+
+    public void setCoverImagePath(String coverImagePath) {
+        this.coverImagePath = coverImagePath;
+    }
+
+    public float getProgress() {
+        return progress;
+    }
+
+    public void setProgress(float progress) {
+        this.progress = progress;
+    }
+
+    protected BookDescription(Parcel in) {
+        id = in.readLong();
+        progress = in.readFloat();
+        title = in.readString();
+        author = in.readString();
+        language = in.readString();
+        type = in.readString();
+        filePath = in.readString();
+        itsFavorite = in.readInt() == 1;
+        coverImagePath = in.readString();
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeLong(id);
+        dest.writeFloat(progress);
+        dest.writeString(title);
+        dest.writeString(author);
+        dest.writeString(language);
+        dest.writeString(type);
+        dest.writeString(filePath);
+        dest.writeInt(itsFavorite ? 1 : 0);
+        dest.writeString(coverImagePath);
+    }
+
+    @SuppressWarnings("unused")
+    public static final Parcelable.Creator<BookDescription> CREATOR = new Parcelable.Creator<BookDescription>() {
+        @Override
+        public BookDescription createFromParcel(Parcel in) {
+            return new BookDescription(in);
+        }
+
+        @Override
+        public BookDescription[] newArray(int size) {
+            return new BookDescription[size];
+        }
+    };
+}
